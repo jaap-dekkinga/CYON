8cdb5c385f4ff2db05b71709ea062e4bb9146da1
Jaap Dekkinga <jaap@tuneurl.com>
2026-06-07 11:12:21 -0400

    Initial Commit

---
+++ CYON/CYONApp.swift
@@ -0,0 +1,17 @@
+//
+//  CYONApp.swift
+//  CYON
+//
+//  Created by Sarah Dekkinga on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CYONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CYONTests/CYONTests.swift
@@ -0,0 +1,17 @@
+//
+//  CYONTests.swift
+//  CYONTests
+//
+//  Created by Sarah Dekkinga on 6/7/26.
+//
+
+import Testing
+@testable import CYON
+
+struct CYONTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
