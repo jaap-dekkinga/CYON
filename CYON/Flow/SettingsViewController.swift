e50c8606f7ef195c2d7408883f174d433aef88ef
Jaap Dekkinga <jaap.dekkinga@gmail.com>
2026-07-22 15:08:34 -0400

    Add debugSelfPlay action to SettingsViewController

--- CYON/Flow/SettingsViewController.swift
+++ CYON/Flow/SettingsViewController.swift
@@ -23,4 +23,19 @@ class SettingsViewController: UIViewController {
 		// TODO: implement this
 	}
 
+	@IBAction func debugSelfPlay(_ sender: Any) {
+	    guard let url = Bundle.main.url(forResource: "Trigger-Sound", withExtension: "wav") else {
+	        NSLog("[CYON-TURL] SELF-TEST: trigger file missing from bundle")
+	        return
+	    }
+	    NSLog("[CYON-TURL] SELF-TEST: playing trigger sound as episode")
+	    Player.shared.currentFileURL = url
+	    Detector.processAudio(for: url) { matches in
+	        NSLog("[CYON-TURL] SELF-TEST DONE matches=\(matches.count)")
+	        for (i, m) in matches.enumerated() {
+	            NSLog("[CYON-TURL] SELF-TEST match[\(i)] time=\(m.time)s matchPct=\(m.matchPercentage)")
+	        }
+	    }
+	}
 }
+
