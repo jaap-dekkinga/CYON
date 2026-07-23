3758ea28a2c74d3f9b5b5ba5621e1c423551cfa2
Jaap Dekkinga <jaap.dekkinga@gmail.com>
2026-07-23 07:53:13 -0400

    Refactor SettingsViewController actions and formatting

--- CYON/Flow/SettingsViewController.swift
+++ CYON/Flow/SettingsViewController.swift
@@ -7,36 +7,19 @@
 //
 
 import UIKit
-import TuneURL
 
 class SettingsViewController: UIViewController {
 
-	@IBOutlet var autoDeleteSwitch: UISwitch!
-	@IBOutlet var autoDownloadSwitch: UISwitch!
+    @IBOutlet var autoDeleteSwitch: UISwitch!
+    @IBOutlet var autoDownloadSwitch: UISwitch!
 
-	// MARK: - Actions
+    // MARK: - Actions
 
-	@IBAction func autoDeleteChanged(_ sender: UISwitch) {
-		// TODO: implement this
-	}
+    @IBAction func autoDeleteChanged(_ sender: UISwitch) {
+        // TODO: implement this
+    }
 
-	@IBAction func autoDownloadChanged(_ sender: UISwitch) {
-		// TODO: implement this
-	}
-
-	@IBAction func debugSelfPlay(_ sender: Any) {
-	    guard let url = Bundle.main.url(forResource: "Trigger-Sound", withExtension: "wav") else {
-	        NSLog("[CYON-TURL] SELF-TEST: trigger file missing from bundle")
-	        return
-	    }
-	    NSLog("[CYON-TURL] SELF-TEST: playing trigger sound as episode")
-	    Player.shared.currentFileURL = url
-	    Detector.processAudio(for: url) { matches in
-	        NSLog("[CYON-TURL] SELF-TEST DONE matches=\(matches.count)")
-	        for (i, m) in matches.enumerated() {
-	            NSLog("[CYON-TURL] SELF-TEST match[\(i)] time=\(m.time)s matchPct=\(m.matchPercentage)")
-	        }
-	    }
-	}
+    @IBAction func autoDownloadChanged(_ sender: UISwitch) {
+        // TODO: implement this
+    }
 }
-
